927c331edf16acbaf3209808ce86c5a2e8a28604
Milben Tan <125988435+bw-mtan@users.noreply.github.com>
2024-10-07 07:53:45 -0400

    Create sizes.d.ts

---
+++ src/scripts/js/vendor/tokens/dist/libs/tokens/sizes.d.ts
@@ -0,0 +1,35 @@
+export type TokensSpace = {
+  0: string;
+  px: string;
+  0.5: string;
+  1: string;
+  2: string;
+  3: string;
+  4: string;
+  5: string;
+  6: string;
+  7: string;
+  8: string;
+  10: string;
+  12: string;
+  16: string;
+  20: string;
+  24: string;
+  32: string;
+  40: string;
+};
+export type TokensRadius = {
+  xs: string;
+  sm: string;
+  md: string;
+  lg: string;
+  xl: string;
+  xxl: string;
+  round: string;
+  pill: string;
+};
+export type TokensSizes = {
+  space: TokensSpace;
+  radius: TokensRadius;
+};
+export declare const sizes: TokensSizes;
